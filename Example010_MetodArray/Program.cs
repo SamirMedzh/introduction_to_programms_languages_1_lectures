b351c469664dab9ab65293a604e831317e8d9302
Samir <camir05r@mail.ru>
2023-02-14 16:28:49 +0300

    add Example010 MetodArray

---
+++ Example010_MetodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 1, 12, 23, 34, 43, 43, 33, 66 };
+
+int n = array.Length;
+int find = 43;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
+
